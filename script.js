804567816efe2298bac190dfe6d5a92b4a773e7b
Alan España <lealanespagna@gmail.com>
2022-09-26 10:52:56 -0500

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("saoko saoko saokoo");
+document.getElementById("generation").style.color = "grey";
+
+function colorTitulo(){ 
+  console.log("pulsó el boton generation!");
+  document.getElementById("generation").style.color = "blue";
+}
